e70e8dcb590f6038d17f1d9939a4d87ebf563a9e
sanketrao411 <sanketraoshinde411@gmail.com>
2025-08-14 10:04:33 +0530

    Update

--- ZZZ/test.cpp
+++ ZZZ/test.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main() 
 {
-   cout << "Sanket" << endl;
+  int num;
+  cin >> num;
+  if(num >= 90){
+    cout << "Grade A" << endl;
+  } else if(num >= 70){
+    cout << "Grade B" << endl;
+  } else if(num >= 50){
+    cout << "Grade C" << endl;
+  } else if(num >= 35){
+    cout << "Grade D" << endl;
+  } else {
+    cout << "Fail" << endl;
+  }
   return 0;
 }
