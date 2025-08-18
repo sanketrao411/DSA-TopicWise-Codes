301ce5b32efbc12198487cda969aedeab3b2ed12
sanketrao411 <sanketraoshinde411@gmail.com>
2025-08-18 10:20:06 +0530

    New Patterns

--- 2. Patterns/11.cpp
+++ 2. Patterns/11.cpp
@@ -6,21 +6,19 @@ int main()
     int n;
     cin >> n;
     
+    bool k = 1;
+
     for (int i = 0; i < n; i++)
     {
+        if(i % 2 == 0) k = 1;
+        else k = 0;
         for (int j = 0; j <= i; j++)
         {
-            cout << "*";
+            cout << k << " ";
+            k = 1 - k;
         }
         cout << endl;
     }
-    for (int i = 0; i < n-1; i++)
-    {
-        for (int j = 0; j < n-i-1; j++)
-        {
-            cout << "*";
-        }
-        cout << endl;
-    }
+    
     return 0;
 }
